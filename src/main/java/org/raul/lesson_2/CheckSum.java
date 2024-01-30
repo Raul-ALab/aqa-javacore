8658040e5af60a4d079ba1f2eef8c65fdc7c5d45
Rashad Raul <rashad.tgr@gmail.com>
2024-01-31 01:45:42 +0400

    Change instance method to static method

--- src/main/java/org/raul/lesson_2/CheckSum.java
+++ src/main/java/org/raul/lesson_2/CheckSum.java
@@ -5,7 +5,7 @@ public class CheckSum {
     /* 1.Написать метод, принимающий на вход два целых числа и проверяющий,
     что их сумма лежит в пределах от 10 до 20 (включительно),
     если да – вернуть true, в противном случае – false. */
-    public boolean checkRangeOfSum() {
+    public static boolean checkRangeOfSum() {
         System.out.print("Enter 1st int value: ");
         int a = GetInput.scan();
         System.out.print("Enter 2nd int value: ");
