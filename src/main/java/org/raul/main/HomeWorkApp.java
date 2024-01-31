2a2401f3f9ee8aea920d98aa2abb533ede8e36e9
Rashad Raul <rashad.tgr@gmail.com>
2024-01-31 15:25:30 +0400

    Complete Leap year method

--- src/main/java/org/raul/main/HomeWorkApp.java
+++ src/main/java/org/raul/main/HomeWorkApp.java
@@ -1,16 +1,15 @@
 package org.raul.main;
 
 
-import org.raul.lesson_2.CheckSum;
-import org.raul.lesson_2.IterateString;
-import org.raul.lesson_2.PositiveNegative;
-import org.raul.lesson_2.PositiveNegativeReturn;
+import org.raul.lesson_2.*;
 
 class HomeWorkApp {
     public static void main(String[] args) {
 //        boolean num = PositiveNegativeReturn.isTrueOrFalse();
 //        System.out.println(num);
 //        IterateString.iterateStringByNumber("Java", 17);
+
+        System.out.println(LeapYear.isLeapYear());
 
     }
 }
