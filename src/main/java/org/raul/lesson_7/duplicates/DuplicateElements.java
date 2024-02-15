e8f679f9a33546fff936961ebaf2e43fbfa4dc2d
Rashad Raul <rashad.tgr@gmail.com>
2024-02-16 01:03:10 +0400

    Use Lombok lib

--- src/main/java/org/raul/lesson_7/duplicates/DuplicateElements.java
+++ src/main/java/org/raul/lesson_7/duplicates/DuplicateElements.java
@@ -1,17 +1,22 @@
 package org.raul.lesson_7.duplicates;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+* 1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
+*
+* Рауль: как посоветовал мой проверяющий, я практикую библиотеку Lombok.
+ * */
+
+@Getter
 public class DuplicateElements<T> {
     private List<T> duplicatesList;
 
     public DuplicateElements() {
         this.duplicatesList = new ArrayList<>();
-    }
-
-    public List<T> getDuplicatesList() {
-        return duplicatesList;
     }
 
     public void addElement(T element) {
