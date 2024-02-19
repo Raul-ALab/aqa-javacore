cc4bc471778dfabe6017d26942cb16979bf80795
Rashad Raul <92177922+RashTGr@users.noreply.github.com>
2024-02-19 17:01:04 +0400

    restructure branches #05

--- src/main/java/org/raul/lesson_5/fruitbox/Orange.java
+++
@@ -1,9 +0,0 @@
-package org.raul.lesson_5.fruitbox;
-
-/* 2.1.Даны классы Fruit, Orange extends Fruit.*/
-public class Orange extends Fruit {
-
-    public Orange(float weight) {
-        super(weight);
-    }
-}
