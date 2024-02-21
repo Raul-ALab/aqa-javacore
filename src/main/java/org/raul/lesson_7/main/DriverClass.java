85e0fa0bbb8468b3464c4b326996c39760fd7f36
Rashad Raul <rashad.tgr@gmail.com>
2024-02-22 01:20:35 +0400

    Complete save method

--- src/main/java/org/raul/lesson_7/main/DriverClass.java
+++ src/main/java/org/raul/lesson_7/main/DriverClass.java
@@ -5,33 +5,17 @@ import org.raul.lesson_7.service.AppData;
 class DriverClass {
     public static void main(String[] args) {
 
-/*        try (OutputStream out = new BufferedOutputStream(
-                new FileOutputStream("src/main/resources/texts/lssn7.csv"))) {
-            for (int i = 0; i < 1000; i++) {
-                out.write(i);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-/*        try (InputStream in = new BufferedInputStream(
-                new FileInputStream("src/main/resources/texts/lssn7.csv"))) {
-            int x;
-            while ((x = in.read()) != -1) {
-                System.out.print((char) x);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
         int header = 3;
-        int row = 3;
+        int row = 2;
         int col = 3;
+        int[] elements = {100, 200, 123, 300, 400, 500};
 
         AppData data = new AppData(header, row, col);
 
         data.setFilePath("src/main/resources/texts/lssn7.csv");
         data.setHeaderTitle("Value");
         data.setDelimiter(';');
+        data.setArrElements(elements);
 
         data.save(data);
 
--- src/main/java/org/raul/lesson_7/service/AppData.java
+++ src/main/java/org/raul/lesson_7/service/AppData.java
@@ -7,22 +7,25 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-@Setter
 public class AppData extends ArrayBuilder {
     private String[] header;
     private int[][] data;
+
+    @Setter
     private String filePath;
+
 
     public AppData(int headerSize, int row, int col) {
         super(headerSize, row, col);
-        header = new String[headerSize];
+        this.header = new String[headerSize];
+        this.data = new int[row][col];
     }
 
-
     public void save(AppData data) {
-        header = initHeader(getHeaderSize());
+        header = initHeader();
+        this.data = initDataArr();
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, false))){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, false))) {
             for (int i = 0; i < header.length; i++) {
                 if (i < header.length - 1) {
                     bw.write(header[i] + " " + (i + 1) + getDelimiter());
@@ -30,8 +33,23 @@ public class AppData extends ArrayBuilder {
                     bw.write(header[i] + " " + (i + 1));
                 }
             }
+            bw.newLine();
+
+            for (int i = 0; i < this.data.length; i++) {
+                for (int j = 0; j < this.data[i].length; j++) {
+                    if (j < this.data[i].length - 1) {
+                        bw.write(Integer.toString(this.data[i][j]) + getDelimiter());
+                    } else {
+                        bw.write(Integer.toString(this.data[i][j]));
+                    }
+                }
+                bw.newLine();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void readFile() {
     }
 }
